834faf8a67a1f46c089a657cc26543ac775a5a8c
IvanParedesA <105755255+IvanParedesA@users.noreply.github.com>
2022-06-11 01:43:17 -0500

    Update mainTransporte.cpp

--- mainTransporte.cpp
+++ mainTransporte.cpp
@@ -1,8 +1,17 @@
-//Iv�n Ricardo Paredes Avilez A01705083
-//03/06/2022
-//mainTransporte.cpp
-//Version 1
+/*
+ *
+ *Proyecto Agencia de Transporte main
+ *Iván Ricardo Paredes Avilez
+ *A01705083
+ *10/06/2022
+ *Version: 2
+ *Este es un programa que nos permite administrar la información
+ *de distintos tipos de viajes que pertenecen a una agencia de Transporte.
+ *Podemos ver los detalles de cada viaje y hacer el calculo de los ingresos
+ *que tenemos en un conjunto de viajes o en su totalidad.
+ */
 
+//Nos permite imprimir
 #include <iostream>
 
 //Importamos nuestras clases creadas en otros archivos
@@ -13,10 +22,21 @@ using namespace std;
 
 int main() {
 
+    //Instanciamos el objeto agencia
     AgenciaTransporte agencia;
+
+    //Generamos lo objteos
     agencia.crea_ejemplos();
+
+    //Impresión de todos los viajes
+    cout << "Estos son todos los viajes: \n" << endl;
     agencia.muestra_viajes();
+
+    //Impresión de solo los viajes de tipo: Taxi Local
+    cout << "Estos son los viajes de tipo Taxi Local: \n" << endl;
     agencia.muestra_viajes("Taxi local");
 
+    //Ingreso total
+    cout << "El ingreso total de la agencia fue: $" << agencia.calc_ingreso_total() << " pesos." << endl;
 
 }
